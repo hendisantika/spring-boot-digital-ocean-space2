1cca1817a9d0f914163332e581a006aabb6118b7
hendisantika <hendisantika@yahoo.co.id>
2024-12-25 08:33:23 +0700

    Add uploadFile in FileController🫸🌀✏️📗🐧🐳⬆

--- src/main/java/id/my/hendisantika/digitaloceanspace2/controller/FileController.java
+++ src/main/java/id/my/hendisantika/digitaloceanspace2/controller/FileController.java
@@ -2,8 +2,17 @@ package id.my.hendisantika.digitaloceanspace2.controller;
 
 import id.my.hendisantika.digitaloceanspace2.service.DigitalOceanSpaceService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
 
 /**
  * Created by IntelliJ IDEA.
@@ -21,4 +30,18 @@ import org.springframework.web.bind.annotation.RestController;
 public class FileController {
 
     private final DigitalOceanSpaceService spaceService;
+
+    // Upload file
+    @PostMapping("/upload")
+    public ResponseEntity<String> uploadFile(@RequestParam("file") MultipartFile file) {
+        try {
+            Path tempFile = Files.createTempFile(file.getOriginalFilename(), ".tmp");
+            file.transferTo(tempFile);
+
+            String fileName = spaceService.uploadFile(tempFile);
+            return new ResponseEntity<>(fileName, HttpStatus.OK);
+        } catch (IOException e) {
+            return new ResponseEntity<>("File upload failed", HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
 }
